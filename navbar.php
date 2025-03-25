fe4370f4b76a1ca445a9ad6a294df1b2e38d1eca
Fathforce <dabix@highclases.tech>
2025-03-25 07:50:50 +0700

    update

--- navbar.php
+++ navbar.php
@@ -1,14 +1,14 @@
     <center>
       <h1>SISTEM ABSENSI</h1>
-	  <h2>MENGGUNAKAN RFID DENGAN ESP32</h2>
+      <h2>MENGGUNAKAN RFID DENGAN FATHSCHOOL</h2>
     </center>
-<!-- Navbar (sit on top) -->
-  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
-    <a href='index.php' class="w3-bar-item w3-button"><b>CT </b> CAPASITORTECH</a>
-    <!-- Float links to the right. Hide them on small screens -->
-    <div class="w3-right w3-hide-small">
-      <a href='update.php' class="w3-bar-item w3-button">Tambahkan Data Baru</a>
-      <a href='absensi.php' class="w3-bar-item w3-button">Absensi Siswa</a>
-	  <a href='datasiswa.php' class="w3-bar-item w3-button">Data Siswa</a>
+    <!-- Navbar (sit on top) -->
+    <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+      <a href='index.php' class="w3-bar-item w3-button"><b>CT </b> FATHFORCE</a>
+      <!-- Float links to the right. Hide them on small screens -->
+      <div class="w3-right w3-hide-small">
+        <a href='update.php' class="w3-bar-item w3-button">Tambahkan Data Baru</a>
+        <a href='absensi.php' class="w3-bar-item w3-button">Absensi Siswa</a>
+        <a href='datasiswa.php' class="w3-bar-item w3-button">Data Siswa</a>
+      </div>
     </div>
-  </div>
